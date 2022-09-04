39e1e966ba2131aa3b07775a5c1c1742dbfc2d5d
ben-green-kerro <ben@kernowrobotics.co.uk>
2022-09-04 19:39:35 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cormoran-driver',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Ben Green',
+    author_email='ben@kernowrobotics.co.uk',
+    description='A package to act as the driver for Cormoran series hardware'
+)
