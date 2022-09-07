6ab30d2429bab332faf8d50db4bca6fba19226f4
Ben <ben@kernowrobotics.co.uk>
2022-09-07 20:07:53 +0100

    changed sim to real

--- cormoran-sim-keyboard.py
+++ cormoran-sim-keyboard.py
@@ -2,7 +2,7 @@ from cormoran import Cormoran2WD
 import time
 robot = Cormoran2WD(['208737A03548', '307F347D3131'],
                     wheelbase=0.0254 * 24, track_width=0.0254 * 24)
-# robot.connect_to_hardware()
+robot.connect_to_hardware()
 robot.start()
 while True:
     robot.input=[0,0]
