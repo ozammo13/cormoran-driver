ea63ea3766fc3b43d633ef3510d7132a092681b2
Ben <ben@kernowrobotics.co.uk>
2022-09-19 16:32:16 +0100

    changed to local control timing

--- cormoran-sim-keyboard.py
+++ cormoran-sim-keyboard.py
@@ -2,14 +2,13 @@ from cormoran import Cormoran2WD
 import time
 robot = Cormoran2WD(['208737A03548', '307F347D3131'],
                     wheelbase=0.0254 * 24, track_width=0.0254 * 24)
-robot.connect_to_hardware()
-robot.start()
+# robot.connect_to_hardware()
+# robot.start()
 while True:
-    robot.input=[0,0]
-    time.sleep(1)
-    robot.input = [25,1]
-    time.sleep(1)
-    robot.input=[0,0]
-    time.sleep(1)
-    robot.input = [-25,-1]
-    time.sleep(1)
+    x=0
+    while x < 200:
+        x+=1
+        robot.inputs=[0.5,1.0]
+        feedback = robot.run_once()
+        print(feedback)
+        time.sleep(1/50)
